5ceeea394e6a1a3c0112e18c00d91019a0eabde0
Berkan <sonmezayberkan@gmail.com>
2022-12-26 20:05:56 +0300

    26.12.2022

---
+++ src/Q01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q01 {
+    public static void main(String[] args) {
+        // Kullanıcıdan alacağını bir stringde boşluk karakterinin olup olmadığını kontrol ediniz.
+        System.out.println("String ifade giriniz: ");
+        Scanner scan=new Scanner(System.in);
+        String deger=scan.nextLine();
+        if(deger!="")
+        System.out.println(deger.contains(" "));
+        // Kullanıcıdan alacağını bir stringin bos olup olmadığını kontrol ediniz.
+        else
+        	System.out.println("herhangi bir değer girilmedi");
+			
+    }
+}
+
---
+++ src/Q06.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q06 {
+    public static void main(String[] args) {
+       /*
+       kullanıcıdan bir e-posta adresi girmesini isteyin, ardından "hotmail" içeriyorsa, "gmail" ile değiştirin,
+        örneğin: johnbrown@hotmail.com ==> johnbrown@gmail.com
+        */
+    	System.out.println("Email adresini giriniz: ");
+    	Scanner scan=new Scanner(System.in);
+    	String email=scan.next();
+    	
+    	if (email.contains("hotmail")) 
+			email=email.replaceAll("hotmail", "gmail");
+		System.out.println("email adresiniz: "+email);
+    }
+}
